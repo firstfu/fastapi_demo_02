8ca0c650371cda2b5209202dae94980d7a5752a6
fu <fu@gmail.com>
2025-09-18 11:14:21 +0800

    新增專案腳本設定，將開發任務指向 tasks:dev

---
+++ tasks.py
@@ -0,0 +1,11 @@
+# tasks.py
+import sys
+
+# from uvicorn import run as uvicorn_run
+
+
+def dev():
+    # 調整成你的 app 模組路徑，例如 fastapi_demo_02.app:app
+    # uvicorn_run("fastapi_demo_02.app:app", reload=True, host="127.0.0.1", port=8000)
+
+    print('demo....................')
